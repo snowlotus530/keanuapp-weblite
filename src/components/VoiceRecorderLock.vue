d991b00da71967ca7d4d21b6a63bca1f86b8cc46
snowlotus530 <snowlotus530@gmail.com>
2021-02-23 22:07:57 +0100

    Voice messages

---
+++ src/components/VoiceRecorderLock.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <div
+    :class="{ 'voice-recorder-lock': true, 'locked': isLocked }"
+    ref="vr_lock"
+  >
+  <div :class="{ 'thumb': true, 'locked': isLocked }" />
+  </div>
+</div>
+</template>
+<script>
+
+export default {
+  name: "VoiceRecorderLock",
+  props: {
+    isLocked: {
+      type: Boolean,
+      default: function () {
+        return false;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/chat.scss";
+</style> 
