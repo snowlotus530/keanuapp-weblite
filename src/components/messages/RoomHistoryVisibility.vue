261eb770c38c2e71a949d71aed017523dc08a982
snowlotus530 <snowlotus530@gmail.com>
2021-03-03 12:29:55 +0100

    Work on read markers
    Don't update read marker for outgoing events (they might be local echoes etc).

---
+++ src/components/messages/RoomHistoryVisibility.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- ROOM AVATAR CHANGED -->
+  <div class="statusEvent">
+    {{ stateEventDisplayName(event) }} made room history {{ history(event) }}
+  </div>
+</template>
+
+<script>
+import messageMixin from "./messageMixin";
+
+export default {
+  mixins: [messageMixin],
+  methods: {
+    history(event) {
+      const visibility = event.getContent().history_visibility;
+      switch (visibility) {
+        case "world_readable":
+          return "readable by anyone";
+        case "shared":
+          return "readable to all members in the room";
+        case "invited":
+          return "readable to members from when they were invited";
+        case "joined":
+          return "readable to members from when they joined";
+      }
+      return visibility;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/chat.scss";
+</style>
