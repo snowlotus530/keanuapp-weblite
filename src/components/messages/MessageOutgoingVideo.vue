cd29f8d681f2581f3eb275cd294ab10acff65760
N-Pex <npexdev@gmail.com>
2021-03-17 12:13:53 +0100

    Support send/receive of videos
    Naive approach, just downloading the whole chunk every time! Need to figure out caching and streaming for long-play stuff.

---
+++ src/components/messages/MessageOutgoingVideo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="messageOut">
+    <div class="op-button" ref="opbutton">
+      <v-btn icon @click.stop="showContextMenu($refs.opbutton)"
+        ><v-icon>more_vert</v-icon></v-btn
+      >
+    </div>
+    <div class="bubble image-bubble">
+      <v-responsive :aspect-ratio="16 / 9" class="ma-0 pa-0">
+        <video :src="src" controls style="width:100%;height:100%">Video file</video>
+      </v-responsive>
+            <QuickReactions :event="event" :reactions="reactions" />
+    </div>
+    <v-avatar class="avatar" size="32" color="#ededed" @click.stop="ownAvatarClicked">
+      <img v-if="userAvatar" :src="userAvatar" />
+      <span v-else class="white--text headline">{{ userAvatarLetter }}</span>
+    </v-avatar>
+    <div class="senderAndTime">
+      <!-- <div class="sender">{{ "You" }}</div> -->
+      <div class="time">
+        {{ formatTime(event.event.origin_server_ts) }}
+      </div>
+      <div class="status">{{ event.status }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import messageMixin from "./messageMixin";
+import attachmentMixin from "./attachmentMixin";
+
+export default {
+  mixins: [messageMixin, attachmentMixin],
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/chat.scss";
+</style>
