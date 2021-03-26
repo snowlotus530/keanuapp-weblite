9d27dc52315cbbb4fccc34a161843559937270c7
snowlotus530 <snowlotus530@gmail.com>
2021-03-26 12:16:13 +0100

    Add switch for join rule
    For issue #86.

---
+++ src/components/messages/RoomJoinRules.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- ROOM JOIN RULES CHANGED -->
+  <div class="statusEvent">
+    {{ stateEventDisplayName(event) }} made the room {{ joinRule(event) }}
+  </div>
+</template>
+
+<script>
+import messageMixin from "./messageMixin";
+
+export default {
+  mixins: [messageMixin],
+  methods: {
+    joinRule(event) {
+      const joinRule = event.getContent().join_rule;
+      switch (joinRule) {
+        case "invite":
+          return "invite only";
+        case "public":
+          return "public";
+      }
+      return joinRule;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/chat.scss";
+</style>
