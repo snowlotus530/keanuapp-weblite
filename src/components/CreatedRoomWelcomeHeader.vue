ebceb450f9cadfd33623637fe7d4c02b7c1ef162
N-Pex <npexdev@gmail.com>
2021-06-22 15:46:37 +0200

    Update welcome message
    Issue #141.

--- src/components/CreatedRoomWelcomeHeader.vue
+++ src/components/CreatedRoomWelcomeHeader.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="created-room-welcome-header">
-    <div class="h4">{{ $t("room_welcome.welcome") }}</div>
-    <div class="mt-2">{{ $t("room_welcome.info") }}</div>
-    <div class="mt-2" v-if="roomIsEncrypted">
-      {{ $t("room_welcome.encrypted") }}
-    </div>
-    <div class="mt-2" v-if="roomHistoryDescription">
-      {{ roomHistoryDescription }}
-    </div>
+    <div>{{ $t("room_welcome.info") }}</div>
     <div class="mt-2" v-if="roomJoinRule == 'public'">
       <i18n path="room_welcome.join_public" tag="span">
         <template v-slot:link>
@@ -19,8 +12,14 @@
       {{ $t("room_welcome.join_invite") }}
     </div>
     <div class="mt-2">{{ $t("room_welcome.info_permissions") }}</div>
+    <div class="mt-2" v-if="roomIsEncrypted">
+      {{ $t("room_welcome.encrypted") }}
+    </div>
+    <div class="mt-2" v-if="roomHistoryDescription">
+      {{ roomHistoryDescription }}
+    </div>
     <div class="text-right">
-      <v-btn text @click.stop="$emit('close')">{{
+      <v-btn text @click.stop="$emit('close')" style="text-transform: none">{{
         $t("room_welcome.got_it")
       }}</v-btn>
     </div>
@@ -50,9 +49,7 @@ export default {
             type: this.$t("message.room_history_invited"),
           });
         case "joined":
-          return this.$t("room_welcome.room_history_is", {
-            type: this.$t("message.room_history_joined"),
-          });
+          return this.$t("room_welcome.room_history_joined");
       }
       return null;
     },
